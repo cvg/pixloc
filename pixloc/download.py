aa2f5c6bf8d8f1f4e1db212d19e23fe25aa7da2d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 00:29:10 +0000

    Adding numeric_owner as keyword arguement

--- pixloc/download.py
+++ pixloc/download.py
@@ -103,7 +103,7 @@ def extract_tar(tarpath: Path, extract_path: Optional[Path] = None,
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(f, extract_path)
